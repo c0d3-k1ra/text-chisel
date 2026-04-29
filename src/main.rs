4e592eeaf8f2357e5f9de18adf71aa8ef3cddfcc
Amit Upadhyay <amit.upadhyay021@outlook.com>
2026-04-30 01:32:47 +0530

    add menu bar icon with template image support for light/dark mode

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,31 @@ use hotkey::HotKeyEvent;
 use tao::event::Event;
 use tao::event_loop::{ControlFlow, EventLoop};
 
+fn load_icon() -> tray_icon::Icon {
+    let svg = include_str!("../assets/icon.svg");
+    let opt = resvg::usvg::Options::default();
+    let tree = resvg::usvg::Tree::from_str(svg, &opt).unwrap();
+    let mut pixmap = resvg::tiny_skia::Pixmap::new(44, 44).unwrap();
+    resvg::render(&tree, resvg::tiny_skia::Transform::default(), &mut pixmap.as_mut());
+    tray_icon::Icon::from_rgba(pixmap.data().to_vec(), 44, 44).unwrap()
+}
+
 fn main() {
     dotenvy::dotenv().ok();
     if std::env::var("ANTHROPIC_API_KEY").is_err() {
         eprintln!("error: ANTHROPIC_API_KEY is not set");
         std::process::exit(1);
     }
+
     let rx = hotkey::run();
     let rt = tokio::runtime::Runtime::new().unwrap();
+
+    let _tray = tray_icon::TrayIconBuilder::new()
+        .with_icon(load_icon())
+        .with_icon_as_template(true)
+        .with_tooltip("text-chisel")
+        .build()
+        .unwrap();
 
     let event_loop = EventLoop::new();
     event_loop.run(move |_event, _, control_flow| {
@@ -43,7 +60,6 @@ fn main() {
             }
         }
 
-        if let Event::WindowEvent { .. } = _event {
-        }
+        if let Event::WindowEvent { .. } = _event {}
     });
 }
